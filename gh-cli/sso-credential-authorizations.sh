3646421c6cad716f788db671a1b2d3c6b6c16b3f
Josh Johanning <joshjohanning@github.com>
2022-01-05 18:29:54 -0600

    adding sso credential gh cli command

---
+++ gh-cli/sso-credential-authorizations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# more info: 
+# - https://github.blog/changelog/2019-04-09-credential-authorizations-api/
+# - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
+
+gh api --paginate /orgs/{org}/credential-authorizations --jq='.[] | [.login]'
