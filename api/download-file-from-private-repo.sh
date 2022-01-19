ef8bac857d91de3befe22e01ea8dc913ab45c37e
Josh Johanning <joshjohanning@github.com>
2022-01-19 13:27:39 -0600

    fixing

--- api/download-file-from-private-repo.sh
+++ api/download-file-from-private-repo.sh
@@ -2,4 +2,4 @@
 
 curl -LX GET 'https://raw.githubusercontent.com/joshjohanning-org/composite-caller-1/main/README.md' \
 --header 'Accept: application/vnd.github.v3+json' \
---header 'Authorization: Bearer ***REMOVED***' -O
+--header 'Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}' -O
