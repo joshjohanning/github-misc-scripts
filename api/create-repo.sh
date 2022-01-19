4f77c48c138942e87b4d614fcf8566c5787e3bd9
Josh Johanning <joshjohanning@github.com>
2022-01-19 14:24:31 -0600

    fixing

--- api/create-repo.sh
+++ api/create-repo.sh
@@ -2,5 +2,5 @@
 
 curl -LX POST 'https://api.github.com/orgs/joshjohanning-test-api2/repos' \
   --header 'Accept: application/vnd.github.v3+json' \
-  --header 'Authorization: Bearer ***REMOVED***' \
+  --header 'Authorization: Bearer xxx' \
   -d '{"name":"myrepo2","visibility":"internal"}'
