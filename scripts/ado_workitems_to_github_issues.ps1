4c3e928f85b833ce7533f53e444ba86f8d3b7436
Josh Johanning <joshjohanning@github.com>
2022-02-23 13:47:05 -0600

    adding more docs, additional features

--- scripts/ado_workitems_to_github_issues.ps1
+++ scripts/ado_workitems_to_github_issues.ps1
@@ -9,7 +9,28 @@
 #      - You can modify the WIQL if you want to use a different way to migrate work items, such as UNDER [Area Path]
 
 # How to run:
-# ./ado_workitems_to_github_issues.ps1 -ado_pat "xxx" -ado_org "jjohanning0798" -ado_project "PartsUnlimited" -ado_tag "migrate" -gh_pat "xxx" -gh_org "joshjohanning-org" -gh_repo "migrate-ado-workitems"
+# ./ado_workitems_to_github_issues.ps1 -ado_pat "xxx" -ado_org "jjohanning0798" -ado_project "PartsUnlimited" -ado_tag "migrate" -gh_pat "xxx" -gh_org "joshjohanning-org" -gh_repo "migrate-ado-workitems" -gh_update_assigned_to $true -gh_assigned_to_user_suffix "_corp"
+
+#
+# Things it migrates:
+# 1. Title
+# 2. Description (or repro steps + system info for a bug
+# 3. State (if the work item is done / closed, it will be closed in GitHub)
+# 4. It will try to assign the work item to the correct user in GitHub - based on ADO email (-gh_update_assigned_to and -gh_assigned_to_user_suffix options)
+# 5. Migrate acceptance criteria as part of issue body (if present)
+# 6. It also migrates the entire work item as JSON as a comment
+#
+
+# 
+# To Do:
+# 1. Migrate acceptance criteria as part of issue body
+# 2. Create a comment on the Azure DevOps work item that says "Migrated to GitHub Issue #"
+#
+
+#
+# Things it won't ever migrate:
+# 1. Created date/update dates
+# 
 
 [CmdletBinding()]
 param (
@@ -19,7 +40,9 @@ param (
     [string]$ado_tag, # only one tag is supported, would have to add another clause in the $wiql, eg: "migrate")
     [string]$gh_pat,
     [string]$gh_org,
-    [string]$gh_repo
+    [string]$gh_repo,
+    [bool]$gh_update_assigned_to = $false, # try to update the assigned to field in GitHub
+    [string]$gh_assigned_to_user_suffix = "" # the emu suffix, ie: "_corp"
 )
 
 echo "$ado_pat" | az devops login --organization "https://dev.azure.com/$ado_org"
@@ -40,13 +63,26 @@ ForEach($workitem in $query) {
     $details_json = az boards work-item show --id $workitem.id --output json
     $details = $details_json | ConvertFrom-Json
 
+    $description=""
+
+    # bug doesn't have Description field - add repro steps and/or system info
     if ($details.fields.{System.WorkItemType} -eq "Bug") {
-        $description=$details.fields.{Microsoft.VSTS.TCM.ReproSteps} + "`n`n" + $details.fields.{Microsoft.VSTS.TCM.SystemInfo}
+        if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.TCM.ReproSteps})) {
+            $description="## Repro Steps`n`n" + $details.fields.{Microsoft.VSTS.TCM.ReproSteps} + "`n`n"
+        }
+        if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.TCM.SystemInfo})) {
+            $description+="## System Info`n`n" + $details.fields.{Microsoft.VSTS.TCM.SystemInfo} + "`n`n"
+        }
     } else {
         $description=$details.fields.{System.Description}
+        # add in acceptance criteria if it has it
+        if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.Common.AcceptanceCriteria})) {
+            $description+="`n`n## Acceptance Criteria`n`n" + $details.fields.{Microsoft.VSTS.Common.AcceptanceCriteria}
+        }
     }
 
     $description | Out-File -FilePath ./temp_issue_body.txt
+    # $acceptance_criteria | Add-Content -Path ./temp_issue_body.txt
 
     $comment_body_json_beginning | Out-File -FilePath ./temp_comment_body.txt
     $details_json | Add-Content -Path ./temp_comment_body.txt
@@ -57,12 +93,24 @@ ForEach($workitem in $query) {
     $issue_url=gh issue create --body-file ./temp_issue_body.txt --repo "$gh_org/$gh_repo" --title $details.fields.{System.Title} --label $work_item_type
     write-host "issue created: $issue_url"
 
-    $issue_id = ($issue_url -split "/")[-1]
-
-    $comment_url=gh issue comment $issue_id --repo "$gh_org/$gh_repo" --body-file ./temp_comment_body.txt
+    $comment_url=gh issue comment $issue_url --body-file ./temp_comment_body.txt
     write-host "comment created: $comment_url"
-
 
     Remove-Item -Path ./temp_comment_body.txt -ErrorAction SilentlyContinue
     Remove-Item -Path ./temp_issue_body.txt -ErrorAction SilentlyContinue
+
+    # close out the issue if it's closed on the Azure Devops side
+    if ($details.fields.{System.State} -eq "Done" -or $details.fields.{System.State} -eq "Closed") {
+        gh issue close $issue_url
+    }
+
+    # update assigned to in GitHub if the option is set - tries to use ado email to map to github username
+    if ($gh_update_assigned_to -eq $true) {
+        $ado_assignee=$details.fields.{System.AssignedTo}.uniqueName
+        $gh_assignee=$ado_assignee.Split("@")[0]
+        $gh_assignee=$gh_assignee.Replace(".", "-") + $gh_assigned_to_user_suffix
+        write-host "trying to assign to: $gh_assignee"
+        $gh_assignee="joshjohanning"
+        $assigned=gh issue edit $issue_url --add-assignee "$gh_assignee"
+    }
 }
