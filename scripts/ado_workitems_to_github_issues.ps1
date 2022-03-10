d76b8dd913c542871d659a32301f09452365bd61
Josh Johanning <joshjohanning@github.com>
2022-03-10 11:41:52 -0600

    adding in comments, formatting

--- scripts/ado_workitems_to_github_issues.ps1
+++ scripts/ado_workitems_to_github_issues.ps1
@@ -42,7 +42,8 @@ param (
     [string]$gh_org,
     [string]$gh_repo,
     [bool]$gh_update_assigned_to = $false, # try to update the assigned to field in GitHub
-    [string]$gh_assigned_to_user_suffix = "" # the emu suffix, ie: "_corp"
+    [string]$gh_assigned_to_user_suffix = "", # the emu suffix, ie: "_corp"
+    [bool]$gh_add_ado_comments = $false # try to get comments
 )
 
 echo "$ado_pat" | az devops login --organization "https://dev.azure.com/$ado_org"
@@ -57,8 +58,8 @@ Remove-Item -Path ./temp_comment_body.txt -ErrorAction SilentlyContinue
 Remove-Item -Path ./temp_issue_body.txt -ErrorAction SilentlyContinue
 
 ForEach($workitem in $query) {
-    $comment_body_json_beginning="`n`n<details><summary>Original Work Item JSON</summary><p>" + "`n`n" + '```json'
-    $comment_body_json_end="`n" + '```' + "`n</p></details>"
+    $original_workitem_json_beginning="`n`n<details><summary>Original Work Item JSON</summary><p>" + "`n`n" + '```json'
+    $original_workitem_json_end="`n" + '```' + "`n</p></details>"
 
     $details_json = az boards work-item show --id $workitem.id --output json
     $details = $details_json | ConvertFrom-Json
@@ -68,31 +69,73 @@ ForEach($workitem in $query) {
     # bug doesn't have Description field - add repro steps and/or system info
     if ($details.fields.{System.WorkItemType} -eq "Bug") {
         if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.TCM.ReproSteps})) {
-            $description="## Repro Steps`n`n" + $details.fields.{Microsoft.VSTS.TCM.ReproSteps} + "`n`n"
+            $description+="## Repro Steps`n`n" + $details.fields.{Microsoft.VSTS.TCM.ReproSteps} + "`n`n"
         }
         if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.TCM.SystemInfo})) {
             $description+="## System Info`n`n" + $details.fields.{Microsoft.VSTS.TCM.SystemInfo} + "`n`n"
         }
     } else {
-        $description=$details.fields.{System.Description}
+        # $description+="## Description`n`n"
+        $description+=$details.fields.{System.Description}
         # add in acceptance criteria if it has it
         if(![string]::IsNullOrEmpty($details.fields.{Microsoft.VSTS.Common.AcceptanceCriteria})) {
             $description+="`n`n## Acceptance Criteria`n`n" + $details.fields.{Microsoft.VSTS.Common.AcceptanceCriteria}
         }
     }
 
-    $description | Out-File -FilePath ./temp_issue_body.txt
-    # $acceptance_criteria | Add-Content -Path ./temp_issue_body.txt
+    # add in details
+    # $description+="`n`n## Details`n`n**Created date**: $($details.fields.{System.CreatedDate})`n**Created by**: $($details.fields.{System.CreatedBy}.displayName)`n**Changed date**: $($details.fields.{System.ChangedDate})`n**Changed by**: $($details.fields.{System.ChangedBy}.displayName)`n**State**: $($details.fields.{System.State})`n"
 
-    $comment_body_json_beginning | Out-File -FilePath ./temp_comment_body.txt
+    $description | Out-File -FilePath ./temp_issue_body.txt
+
+    $url="[Original Work Item URL](https://dev.azure.com/$ado_org/$ado_project/_workitems/edit/$($workitem.id))"
+    $url | Out-File -FilePath ./temp_comment_body.txt
+
+    # create the details chart
+    $ado_details_beginning="`n`n<details><summary>Original Work Item Details</summary><p>" + "`n`n"
+    $ado_details_beginning | Add-Content -Path ./temp_comment_body.txt
+    $ado_details= "| Created date | Created by | Changed date | Changed By | State | Type | Area Path | Iteration Path|`n|---|---|---|---|---|---|---|---|`n"
+    $ado_details+="| $($details.fields.{System.CreatedDate}) | $($details.fields.{System.CreatedBy}.displayName) | $($details.fields.{System.ChangedDate}) | $($details.fields.{System.ChangedBy}.displayName) | $($details.fields.{System.State}) | $($details.fields.{System.WorkItemType}) | $($details.fields.{System.AreaPath}) | $($details.fields.{System.IterationPath}) |`n`n"
+    $ado_details | Add-Content -Path ./temp_comment_body.txt
+    $ado_details_end="`n" + "`n</p></details>"    
+    $ado_details_end | Add-Content -Path ./temp_comment_body.txt
+
+    # prepare the comment
+    $original_workitem_json_beginning | Add-Content -Path ./temp_comment_body.txt
     $details_json | Add-Content -Path ./temp_comment_body.txt
-    $comment_body_json_end | Add-Content -Path ./temp_comment_body.txt
+    $original_workitem_json_end | Add-Content -Path ./temp_comment_body.txt
+
+    # getting comments if enabled
+    if($gh_add_ado_comments -eq $true) {
+        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+        $base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":$ado_pat"))
+        $headers.Add("Authorization", "Basic $base64")
+        $response = Invoke-RestMethod "https://dev.azure.com/$ado_org/$ado_project/_apis/wit/workItems/$($workitem.id)/comments?api-version=7.1-preview.3" -Method 'GET' -Headers $headers
+        
+        if($response.count -gt 0) {
+            $ado_comments_details=""
+            $ado_original_workitem_json_beginning="`n`n<details><summary>Work Item Comments ($($response.count))</summary><p>" + "`n`n"
+            $ado_original_workitem_json_beginning | Add-Content -Path ./temp_comment_body.txt
+            ForEach($comment in $response.comments) {
+                # $ado_comments_details= "> **Created date**: $($comment.createdDate)`n**Created by**: $($comment.createdBy.displayName)`n**[Comment URL JSON]($($comment.url))**`n**Comment text**:$($comment.text)`n`n-----------`n`n"
+                $ado_comments_details= "| Created date | Created by | JSON URL |`n|---|---|---|`n"
+                $ado_comments_details+="| $($comment.createdDate) | $($comment.createdBy.displayName) | [URL]($($comment.url)) |`n`n"
+                $ado_comments_details+="**Comment text**: $($comment.text)`n`n-----------`n`n"
+                $ado_comments_details | Add-Content -Path ./temp_comment_body.txt
+            }
+            $ado_original_workitem_json_end="`n" + "`n</p></details>"
+            $ado_original_workitem_json_end | Add-Content -Path ./temp_comment_body.txt
+        }
+    }
     
+    # setting the label on the issue to be the work item type
     $work_item_type = $details.fields.{System.WorkItemType}.ToLower()
 
+    # create the issue
     $issue_url=gh issue create --body-file ./temp_issue_body.txt --repo "$gh_org/$gh_repo" --title $details.fields.{System.Title} --label $work_item_type
     write-host "issue created: $issue_url"
 
+    # add the comment
     $comment_url=gh issue comment $issue_url --body-file ./temp_comment_body.txt
     write-host "comment created: $comment_url"
 
