41eee1e9a7c5b0ac9ae821ab6122f8b6b67dcc15
Josh Johanning <joshjohanning@github.com>
2022-04-08 10:58:17 -0500

    adding another example to get commits since

--- gh-cli/get-commits-since-date.sh
+++ gh-cli/get-commits-since-date.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 gh api --paginate --method GET "/repos/joshjohanning-org/azure-key-vault-test/commits?since=2022-04-07T16:00:49Z" 
+
+# or - if you use -f, you have to explicity specify the --method GET otherwise it defaults to POST
+gh api --paginate --method GET “/repos/joshjohanning-org/azure-key-vault-test/commits” -f 'since=2022-04-07T16:00:49Z'
