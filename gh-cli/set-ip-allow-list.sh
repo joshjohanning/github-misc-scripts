ab46f71b8bb2256d2849851413d4ac13c78bcacf
Josh Johanning <joshjohanning@github.com>
2022-11-28 16:09:28 -0600

    fix

--- gh-cli/set-ip-allow-list.sh
+++ gh-cli/set-ip-allow-list.sh
@@ -2,7 +2,7 @@
 
 # use `get-enterprise-id.sh` or `get-organization-id.sh` to get the owner_id
 
-gh api graphql -f owner_id='O_kgDOBbB18g' -f setting_value='ENABLED' -f name='test ip' -f query='
+gh api graphql -f owner_id='O_kgDOBbB18g' -f setting_value='ENABLED' -f query='
 mutation ($owner_id: ID! $setting_value: IpAllowListEnabledSettingValue!) { 
   updateIpAllowListEnabledSetting(input: { ownerId: $owner_id settingValue: $setting_value }) { 
       clientMutationId
