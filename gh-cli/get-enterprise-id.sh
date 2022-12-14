59f5a84afb5cdf9fe98ed67d412f2c19b3a43553
Josh Johanning <joshjohanning@github.com>
2022-12-14 11:04:50 -0600

    updating token scope

--- gh-cli/get-enterprise-id.sh
+++ gh-cli/get-enterprise-id.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# gh cli's token needs to be able to read enterprise - run this first if it can't
+# gh auth refresh -h github.com -s read:enterprise
+
 gh api graphql -H X-Github-Next-Global-ID:1 -f enterprise='my-enterprise-name' -f query='
 query ($enterprise: String!)
   { enterprise(slug: $enterprise) { 
