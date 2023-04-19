79d4984970901aac4765b3e3ba672408b6f0d09b
Josh Johanning <joshjohanning@github.com>
2023-04-19 15:53:04 -0500

    adding credential_id

--- gh-cli/sso-credential-authorizations.sh
+++ gh-cli/sso-credential-authorizations.sh
@@ -4,4 +4,4 @@
 # - https://github.blog/changelog/2019-04-09-credential-authorizations-api/
 # - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
 
-gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    note: \(.authorized_credential_note)"'
+gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    ID: \(.credential_id)    note: \(.authorized_credential_note)"'
