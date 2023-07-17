744fdea61e3aa24c946cb6f5aa05135601a48ef8
Tiago Pascoal <tiago@pascoal.net>
2023-07-17 13:43:48 +0100

    Parameterize get-apps-installed-in-organization
    The org name is no longer hardcoded

--- gh-cli/get-apps-installed-in-organization.sh
+++ gh-cli/get-apps-installed-in-organization.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-gh api /orgs/joshjohanning-org/installations --paginate --jq '.installations[].app_slug'
+if [ -z "$1" ]; then
+  echo "Usage: $0 <org>"
+  exit 1
+fi
+
+org="$1"
+
+gh api "/orgs/$org/installations" --paginate --jq '.installations[].app_slug'
