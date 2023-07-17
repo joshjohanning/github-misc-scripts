9690dd9c09afe7fd4f21b73590b7208284907e70
Tiago Pascoal <tiago@pascoal.net>
2023-07-17 13:38:31 +0100

    Parameterize get-enterprise-organizations
    enterprise slug is no longer hardcoded and can now be passed as a parameter

--- gh-cli/get-enterprise-organizations.sh
+++ gh-cli/get-enterprise-organizations.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-gh api graphql --paginate -f enterpriseName='my-enterprise-name' -f query='
+if [ -z "$1" ]; then
+  echo "Usage: $(basename $0) <enterprise-slug>"
+  exit 1
+fi
+
+enterpriseslug=$1
+
+gh api graphql --paginate -f enterpriseName="$enterpriseslug" -f query='
 query getEnterpriseOrganizations($enterpriseName: String! $endCursor: String) {
   enterprise(slug: $enterpriseName) {
     organizations(first: 100, after: $endCursor) {
