7d5f11bf544857c1a02cfde6d989a3841318ddab
Tiago Pascoal <tiago@pascoal.net>
2023-07-20 16:08:42 +0100

    Use two parameters to be more consistent with other scripts

--- gh-cli/delete-repository-webhooks.sh
+++ gh-cli/delete-repository-webhooks.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <repo>"
-  echo "repo in the format owner/repo"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <owner> <repo>"
   exit 1
 fi
 
-repo=$1
-
-# validate if repo is in format owner/repo
-if [[ ! $repo =~ ^[[:alnum:]_-]+/[[:alnum:]_-]+$ ]]; then
-  echo "ERROR: invalid repo format. Expected owner/repo"
-  exit 1
-fi
+owner=$1
+repo=$2
 
 # iterate on all repo webhooks and delete them
-gh api -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$repo/hooks" --jq '.[].id' | while read -r id; do
+gh api -H "X-GitHub-Api-Version: 2022-11-28" "/repos/$owner/$repo/hooks" --jq '.[].id' | while read -r id; do
   echo "deleting webhook $id"
   gh api -X DELETE "repos/$repo/hooks/$id"
 done 
