2ef71ced703649b83fe3a5cccf9f54095f117d6c
Tiago Pascoal <tiago@pascoal.net>
2023-07-27 12:54:38 +0100

    Parameterize IP allow get ip allow lists (ent and org)
    Output is also formated as a tsv

--- gh-cli/get-enterprise-ip-allow-list.sh
+++ gh-cli/get-enterprise-ip-allow-list.sh
@@ -3,7 +3,14 @@
 # gh cli's token needs to be able to admin enterprise - run this first if it can't
 # gh auth refresh -h github.com -s admin:enterprise
 
-gh api graphql --paginate -f enterpriseName='my-enterprise' -f query='
+if [ $# -lt "1" ]; then
+    echo "Usage: $0 <enterprise-slug>"
+    exit 1
+fi
+
+enterprise=$1
+
+gh api graphql --paginate -f enterpriseName="$enterprise" -f query='
 query getEnterpriseIpAllowList($enterpriseName: String! $endCursor: String) {
   enterprise(slug: $enterpriseName) {
     ownerInfo {
@@ -23,4 +30,4 @@ query getEnterpriseIpAllowList($enterpriseName: String! $endCursor: String) {
       }
     }
   }
-}'
+}' --jq '.data.enterprise.ownerInfo.ipAllowListEntries.nodes[] | [.id, .allowListValue, .name, .isActive, .createdAt, .updatedAt] | @tsv'
