fc643774eb7ac7fb2146e730405d75184716afcd
Josh Johanning <joshjohanning@github.com>
2023-09-08 15:02:40 -0500

    placeholder for nuget script

---
+++ scripts/migrate-nuget-packages-between-github-instances.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# See: https://github.com/joshjohanning/github-packages-migrate-nuget-packages-between-github-instances
+
+echo "See: https://github.com/joshjohanning/github-packages-migrate-nuget-packages-between-github-instances"
