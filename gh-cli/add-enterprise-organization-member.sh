787bddd81644c3a6bb51016fe8a28c389453b2b1
Josh Johanning <joshjohanning@github.com>
2023-09-18 11:33:11 -0500

    parameterizing add organization scripts

--- gh-cli/add-enterprise-organization-member.sh
+++ gh-cli/add-enterprise-organization-member.sh
@@ -7,12 +7,66 @@
 # - MEMBER: The user is a member of the organization.
 # - ADMIN: The user is an administrator/owner of the organization.
 
-gh api graphql -f enterpriseId='E_abc' -f organizationId='O_abc' -f userIds='U_abc' -f role='MEMBER' -f query='
+function print_usage {
+  echo "Usage: $0 <enterprise> <org> <user> <role>"
+  echo "Example: ./get-enterprise-id.sh avocado-corp joshjohanning-org joshjohanning admin"
+  echo "Valid roles: MEMBER, ADMIN"
+  exit 1
+}
+
+if [ -z "$4" ]; then
+  print_usage
+fi
+
+enterprise="$1"
+org="$2"
+user="$3"
+role=$(echo "$4" | tr '[:lower:]' '[:upper:]')
+
+case "$role" in
+  "MEMBER" | "ADMIN")
+    ;;
+  *)
+    print_usage
+    ;;
+esac
+
+enterprise_id=$(gh api graphql -H X-Github-Next-Global-ID:1 -f enterprise="$enterprise" -f query='
+query ($enterprise: String!)
+  { enterprise(slug: $enterprise) {
+    id
+  } 
+}
+' --jq '.data.enterprise.id')
+
+org_id=$(gh api graphql -H X-Github-Next-Global-ID:1 -f organization="$org" -f query='
+query ($organization: String!)
+  { organization(login: $organization) {
+    login
+    name
+    id 
+  }
+}
+' --jq '.data.organization.id')
+
+user_id=$(gh api graphql -H X-Github-Next-Global-ID:1 -f user="$user" -f query='
+query ($user: String!)
+  { user(login: $user) {
+    login
+    name
+    id
+  }
+}
+' --jq '.data.user.id')
+
+gh api graphql -H X-Github-Next-Global-ID:1 -f enterpriseId="$enterprise_id" -f organizationId="$org_id" -f userIds="$user_id" -f role=$role -f query='
 mutation($enterpriseId: ID!, $organizationId: ID!, $userIds: [ID!]!, $role: OrganizationMemberRole) {
   addEnterpriseOrganizationMember(input: {enterpriseId: $enterpriseId, organizationId: $organizationId, userIds: $userIds, role: $role}) {
+    clientMutationId
     users {
-      id
       login
+      name
+      id
     }
   }
 }'
