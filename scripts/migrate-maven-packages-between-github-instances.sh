20464aa9c3b90ed131f3b108c1f0364c7cccaf45
Josh Johanning <joshjohanning@github.com>
2023-09-21 20:53:03 -0500

    updating package scripts

--- scripts/migrate-maven-packages-between-github-instances.sh
+++ scripts/migrate-maven-packages-between-github-instances.sh
@@ -6,9 +6,16 @@
 # Prereqs:
 # 1. [gh cli](https://cli.github.com) installed
 # 2. Set the source GitHub PAT env var: `export GH_SOURCE_PAT=ghp_abc` (must have at least `read:packages`, `read:org` scope)
-# 3. Set the target GitHub PAT env var: `export GH_TARGET_PAT=ghp_xyz` (must have at least `write:packages`, `read:org` scope)
+# 3. Set the target GitHub PAT env var: `export GH_TARGET_PAT=ghp_xyz` (must have at least `write:packages`, `read:org`, `repo` scope)
 #
 # Example: ./migrate-maven-packages-between-github-instances.sh joshjohanning-org github.com joshjohanning-emu github.com
+#
+# Notes:
+# - Until Maven supports the new GitHub Packages type, mvnfeed requires the target repo to exist 
+# - This scripts creates the repo if it doesn't exist
+# - Otherwise, if the repo doesn't exist, receive "example-1.0.5.jar was not found in the repository" error
+# - Link to [GitHub public roadmap item](https://github.com/github/roadmap/issues/578)
+#
 
 set -e
 
@@ -59,7 +66,7 @@ then
   cd $temp_dir
 fi
 
-# get current user
+# get current user for source/target
 current_user_source=$(curl -s -H "Authorization: Bearer $GH_SOURCE_PAT" https://api.$SOURCE_HOST/user | jq -r '.login')
 current_user_target=$(curl -s -H "Authorization: Bearer $GH_TARGET_PAT" https://api.$TARGET_HOST/user | jq -r '.login')
 
@@ -76,10 +83,6 @@ echo "$packages" | while IFS= read -r response; do
 
   echo "org: $SOURCE_ORG repo: $repo_name --> package name $package_name"
 
-  # set up mvnfeed registries
-  # mvnfeed config repo add --name github_source --username $current_user --url https://maven.pkg.github.com/$SOURCE_ORG/$repo_name
-  # mvnfeed config repo add --name github_111 --username $current_user --url https://maven.pkg.github.com/$TARGET_ORG/$repo_name
-  
   # set up source and target registries for mvnfeed
   mvnfeed config repo list >/dev/null 2>&1
   rm ~/.mvnfeed/mvnfeed.ini
@@ -88,52 +91,32 @@ echo "$packages" | while IFS= read -r response; do
   echo "url = https://maven.pkg.github.com/$SOURCE_ORG/$repo_name" >> ~/.mvnfeed/mvnfeed.ini
   echo "authorization = Basic $auth_source" >> ~/.mvnfeed/mvnfeed.ini
   echo "" >> ~/.mvnfeed/mvnfeed.ini
-
   echo "[repository.githubtarget]" >> ~/.mvnfeed/mvnfeed.ini
   echo "url = https://maven.pkg.github.com/$TARGET_ORG/$repo_name" >> ~/.mvnfeed/mvnfeed.ini
   echo "authorization = Basic $auth_target" >> ~/.mvnfeed/mvnfeed.ini
   echo "" >> ~/.mvnfeed/mvnfeed.ini
 
   mvnfeed config stage_dir set --path $temp_dir/artifacts
-  
+
+  # check if $TARGET_ORG/$repo_name exists in GitHub - if not, create it
+  if ! GH_HOST="$TARGET_HOST" gh api "/repos/$TARGET_ORG/$repo_name" >/dev/null 2>&1
+  then
+    echo "creating repo $TARGET_ORG/$repo_name"
+    GH_HOST="$TARGET_HOST" gh repo create "$TARGET_ORG/$repo_name" --private --confirm
+  fi
+
   versions=$(GH_HOST="$SOURCE_HOST" gh api --paginate "/orgs/$SOURCE_ORG/packages/maven/$package_name/versions" -q '.[] | .name' | sort -V)
   for version in $versions
   do
     echo "$version"
-    # get package name before first period
     package_com=$(echo "$package_name" | cut -d '.' -f 1)
-    echo $package_com
-    # get package name after first period but before third period
-    package_group=$(echo "$package_name" | cut -d '.' -f 2)
-    echo $package_group
-    package_artifact=$(echo "$package_name" | cut -d '.' -f 3)
-    echo $package_artifact
+    package_group=$(echo "$package_name" | cut -d '.' -f 2- | rev | cut -d '.' -f 2- | rev)
+    package_artifact=$(echo "$package_name" | rev | cut -d '.' -f 1 | rev)
 
     name=$(echo $package_com.$package_group:$package_artifact:$version)
     echo $name
 
-    mvnfeed artifact transfer --from=githubsource --to=githubtarget --name=$package_com.$package_group:$package_artifact:$version --debug
-
-    exit 2
-    # url=$(curl -H "Authorization: token $GH_SOURCE_PAT" -Ls https://maven.pkg.github.com/$SOURCE_ORG/download/$package_com/$package_group/$package_artifact/$version/$package_artifact-$version | jq --arg version $version -r '.versions[$version].dist.tarball')
-
-    # # check for error
-    # if [ "$url" == "null" ]; then
-    #     echo "ERROR: version $version not found for package $package_name"
-    #     echo "NOTE: Make sure you have the proper scopes for gh; ie run this: gh auth refresh -h github.com -s read:packages"
-    #     exit 1
-    # fi
-
-    # # download 
-    # curl -H "Authorization: token $GH_SOURCE_PAT" -L -o $package_name-$version.tgz $url
-  
-    # # untar
-    # mkdir -p ./$package_name-$version
-    # tar xzf $package_name-$version.tgz -C $package_name-$version
-    # cd $package_name-$version/package
-    # perl -pi -e "s/$SOURCE_ORG/$TARGET_ORG/g" package.json
-    # npm publish --userconfig $temp_dir/.npmrc
-    # cd ./../../
+    mvnfeed artifact transfer --from=githubsource --to=githubtarget --name=$package_com.$package_group:$package_artifact:$version
 
   done
 
--- scripts/migrate-npm-packages-between-github-instances.sh
+++ scripts/migrate-npm-packages-between-github-instances.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Usage: ./migrate-nuget-packages-between-orgs.sh <source-org> <source-host> <target-org> <target-host>
+# Usage: ./migrate-npm-packages-between-github-instances.sh <source-org> <source-host> <target-org> <target-host>
 #
 #
 # Prereqs:
-# 1. Define the source GitHub PAT env var: export GH_SOURCE_PAT=ghp_abc (must have at least `read:packages`, `read:org` scope)
-# 2. Define the target GitHub PAT env var: export GH_TARGET_PAT=ghp_abc (must have at least `write:packages`, `read:org` scope)
+# 1. [gh cli](https://cli.github.com) installed
+# 2. Set the source GitHub PAT env var: `export GH_SOURCE_PAT=ghp_abc` (must have at least `read:packages`, `read:org` scope)
+# 3. Set the target GitHub PAT env var: `export GH_TARGET_PAT=ghp_xyz` (must have at least `write:packages`, `read:org` scope)
 #
 # Example: ./migrate-npm-packages-between-github-instances.sh joshjohanning-org github.com joshjohanning-emu github.com
 
-if [ -z "$4" ]; then
+if [ $# -ne "4" ]; then
     echo "Usage: $0 <source-org> <source-host> <target-org> <target-host>"
     exit 1
 fi
 
 # make sure env variables are defined
 if [ -z "$GH_SOURCE_PAT" ]; then
-    echo "define GH_SOURCE_PAT env var"
+    echo "Error: set GH_SOURCE_PAT env var"
     exit 1
 fi
 
 if [ -z "$GH_TARGET_PAT" ]; then
-    echo "define GH_TARGET_PAT env var"
+    echo "Error: set GH_TARGET_PAT env var"
     exit 1
 fi
 
@@ -32,8 +33,8 @@ SOURCE_HOST=$2
 TARGET_ORG=$3
 TARGET_HOST=$4
 
-# log into source
-gh auth login -h $SOURCE_HOST --with-token <<< $GH_SOURCE_PAT
+# log in to gh cli with source pat
+export GH_TOKEN=$GH_SOURCE_PAT
 
 # create temp dir
 mkdir -p ./temp
@@ -85,6 +86,3 @@ echo "$packages" | while IFS= read -r response; do
 done
 
 echo "Run this to clean up your working dir: rm -rf ./temp"
-
-# log out of gh - optional
-# gh auth logout -h $SOURCE_HOST
