9b97dbf56a1fc576145afc4a82bbfc0d4c238fef
Josh Johanning <joshjohanning@github.com>
2023-12-20 10:39:25 -0600

    feat: change name to login, add script parameters

--- gh-cli/get-enterprise-roles-in-organizations-all-roles.sh
+++ gh-cli/get-enterprise-roles-in-organizations-all-roles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this gets the enterprise roles for the current user, e.g. which organizations they are a member/owner of
+
 # gh cli's token needs to be able to admin enterprise - run this first if it can't
 # gh auth refresh -h github.com -s admin:enterprise
 
@@ -7,12 +9,20 @@
 # "viewerIsAMember": will return true if the viewer is a member of the organization
 # "viewerCanAdminister": will return true if the viewer is a admin of the organization - will return true for `viewIsAMember` as well
 
-gh api graphql --paginate -f enterpriseSlug='avocado-corp' -f query='
+if [ -z "$1" ]; then
+  echo "Usage: $0 <enterprise>"
+  echo "Example: ./get-enterprise-roles-in-organizations-all-roles.sh avocado-corp"
+  exit 1
+fi
+
+enterprise="$1"
+
+gh api graphql --paginate -f enterpriseSlug=$enterprise -f query='
 query ($enterpriseSlug: String!, $endCursor: String) {
   enterprise(slug: $enterpriseSlug) {
     organizations(first: 100, after: $endCursor) {
       nodes {
-        name
+        login
         viewerIsAMember
         viewerCanAdminister
       }
