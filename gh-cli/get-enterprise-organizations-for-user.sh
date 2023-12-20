46104fa50bfbbea20f14536ab21db1ff237f0514
Josh Johanning <joshjohanning@github.com>
2023-12-20 10:51:49 -0600

    fix: pagination for enterprise organizations for user script

--- gh-cli/get-enterprise-organizations-for-user.sh
+++ gh-cli/get-enterprise-organizations-for-user.sh
@@ -5,13 +5,9 @@
 # gh cli's token needs to be able to admin enterprise - run this first if it can't
 # gh auth refresh -h github.com -s admin:enterprise
 
-# results
-# "viewerIsAMember": will return true if the viewer is a member of the organization
-# "viewerCanAdminister": will return true if the viewer is a admin of the organization - will return true for `viewIsAMember` as well
-
 if [ -z "$2" ]; then
   echo "Usage: $0 <enterprise> <user>"
-  echo "Example: ./get-enterprise-organizations-by-user joshjohanning"
+  echo "Example: ./get-enterprise-organizations-for-user.sh joshjohanning"
   exit 1
 fi
 
@@ -21,21 +17,21 @@ user="$2"
 gh api graphql --paginate -f enterpriseSlug=$enterprise -f login=$user -f query='
 query ($enterpriseSlug: String!, $login: String!, $endCursor: String) {
   enterprise(slug: $enterpriseSlug) {
-    members(query: $login, first: 100, after: $endCursor) {
+    members(query: $login, first: 1) {
       nodes {
         ... on EnterpriseUserAccount {
             login
-            organizations(first: 100) {
+            organizations(first: 100, after: $endCursor) {
               nodes {
                 login
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
               }
             }
         }
       }
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
     }
   }
-}' | jq -r '{login: .data.enterprise.members.nodes[0].login, organizations: [.data.enterprise.members.nodes[0].organizations.nodes[].login]}'
+}' | jq -s -r '{login: .[0].data.enterprise.members.nodes[0].login, organizations: [.[].data.enterprise.members.nodes[0].organizations.nodes[].login]}'
