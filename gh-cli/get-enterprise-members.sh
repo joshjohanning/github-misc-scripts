e73b92eafa2fa1f0e9302a15cbf432799499abb0
Josh Johanning <joshjohanning@github.com>
2024-02-06 21:14:55 -0600

    feat: parameterize and add emails

--- gh-cli/get-enterprise-members.sh
+++ gh-cli/get-enterprise-members.sh
@@ -4,7 +4,15 @@
 # gh cli's token needs to be able to admin enterprise - run this first if it can't
 # gh auth refresh -h github.com -s admin:enterprise
 
-gh api graphql --paginate -f enterpriseSlug='avocado-corp'  -f query='
+if [ -z "$1" ]; then
+  echo "Usage: $0 <enterprise>"
+  echo "Example: ./get-enterprise-members.sh avocado-corp"
+  exit 1
+fi
+
+enterprise="$1"
+
+gh api graphql --paginate -f enterpriseSlug=$enterprise -f query='
 query ($enterpriseSlug: String!,  $endCursor: String) {
   enterprise(slug: $enterpriseSlug) {
     members(first: 100, after: $endCursor) {
@@ -12,6 +20,9 @@ query ($enterpriseSlug: String!,  $endCursor: String) {
         ... on EnterpriseUserAccount {
             login
             name
+            user {
+              email
+            }
         }
       }
       pageInfo {
