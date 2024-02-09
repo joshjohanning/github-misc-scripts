824319738bac2440c578988167231a05d0e8785b
Josh Johanning <joshjohanning@github.com>
2024-02-09 11:26:41 -0600

    chore: update readme

--- gh-cli/get-sso-credential-authorizations.sh
+++ gh-cli/get-sso-credential-authorizations.sh
@@ -4,6 +4,6 @@
 # - https://github.blog/changelog/2019-04-09-credential-authorizations-api/
 # - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
 
-# gets both "personal access token" and "SSH key" credential types
+# gets both "personal access token" and "SSH key" credential types and their expiration (if applicable)
 
 gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    ID: \(.credential_id)    note: \(.authorized_credential_note) type: \(.credential_type)"'
