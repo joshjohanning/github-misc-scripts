ec7508a98ee4da6a9f92a9bed66989366abcdf0a
Josh Johanning <joshjohanning@github.com>
2024-03-06 17:10:02 -0600

    feat: update sso ssh key fields

--- gh-cli/get-sso-enabled-ssh-keys.sh
+++ gh-cli/get-sso-enabled-ssh-keys.sh
@@ -5,4 +5,4 @@
 # - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
 
 # credential_type: personal access token, SSH key
-gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | select(.credential_type == "SSH key") | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    ID: \(.credential_id)    note: \(.authorized_credential_note)    type: \(.credential_type)"'
+gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | select(.credential_type == "SSH key") | "login: \(.login)    ID: \(.credential_id)    title: \(.authorized_credential_title)    type: \(.credential_type)"'
