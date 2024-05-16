0746e28eccfc7c70d63e658a253c4603350252f6
Josh Johanning <joshjohanning@github.com>
2024-05-16 11:43:14 -0500

    feat: enhance sso creds scripts and use json outputs by default

--- gh-cli/get-sso-credential-authorizations.sh
+++ gh-cli/get-sso-credential-authorizations.sh
@@ -3,7 +3,11 @@
 # more info: 
 # - https://github.blog/changelog/2019-04-09-credential-authorizations-api/
 # - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
+# - https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/orgs?apiVersion=2022-11-28#list-saml-sso-authorizations-for-an-organization
 
-# gets both "personal access token" and "SSH key" credential types and their expiration (if applicable)
+# gets all credential types ("personal access token", "SSH key", "OAuth app token", and "GitHub app token") and their expiration (if applicable)
 
-gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    ID: \(.credential_id)    note: \(.authorized_credential_note) type: \(.credential_type)"'
+gh api --paginate /orgs/githubcustomers/credential-authorizations
+
+# old - raw text output, 1 per line
+# gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | "login: \(.login)    expiration: \(.authorized_credential_expires_at)    ID: \(.credential_id)    note: \(.authorized_credential_note) type: \(.credential_type)"'
--- gh-cli/get-sso-enabled-ssh-keys.sh
+++ gh-cli/get-sso-enabled-ssh-keys.sh
@@ -3,6 +3,10 @@
 # more info: 
 # - https://github.blog/changelog/2019-04-09-credential-authorizations-api/
 # - https://github.blog/changelog/2021-11-09-expiration-dates-of-saml-authorized-pats-available-via-api/
+# - https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/orgs?apiVersion=2022-11-28#list-saml-sso-authorizations-for-an-organization
 
-# credential_type: personal access token, SSH key
-gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | select(.credential_type == "SSH key") | "login: \(.login)    ID: \(.credential_id)    title: \(.authorized_credential_title)    type: \(.credential_type)"'
+# credential_type: personal access token, SSH key, OAuth app token, GitHub app token
+gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | select(.credential_type == "SSH key")'
+
+# old - raw text output, 1 per line
+# gh api --paginate /orgs/githubcustomers/credential-authorizations --jq='.[] | select(.credential_type == "SSH key") | "login: \(.login)    ID: \(.credential_id)    title: \(.authorized_credential_title)    type: \(.credential_type)"'
