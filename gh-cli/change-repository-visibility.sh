0b600f310d0e6a850861d7da6c8657d7315bae3e
Josh Johanning <joshjohanning@github.com>
2024-10-03 15:51:56 -0500

    feat: misc improvements add hostname add #!/bin/bash parameterize inputs

--- gh-cli/change-repository-visibility.sh
+++ gh-cli/change-repository-visibility.sh
@@ -1,9 +1,23 @@
-# using gh repo edit native cli:
-gh repo edit <org>/<repo> --visibility internal
+#!/bin/bash
+
+# changes the visibility of a repository
+
+if [ $# -lt "3" ]; then
+    echo "Usage: $0 <org> <repo> <visibility: public|internal|private> <optional: hostname>"
+    exit 1
+fi
+
+org=$1
+repo=$2
+visibility=$3
+hostname=${4:-"github.com"}
 
 # alternative with gh api (can be used to modify multiple properties of repo at once):
-gh api \
-  --method PATCH \
-  -H "Accept: application/vnd.github+json" \
-  /repos/<org>/<repo> \
- -f visibility='internal'
+result=$(gh api --hostname $hostname -X PATCH /repos/$org/$repo -f visibility=$visibility | jq -r '"name: \(.name), visibility: \(.visibility)"')
+
+if [ $? -eq 0 ]; then
+  echo "$result"
+fi
+
+# using gh repo edit native cli:
+# gh repo edit $org/$repo --visibility $visibility
--- gh-cli/get-organizations-settings.sh
+++ gh-cli/get-organizations-settings.sh
@@ -9,25 +9,18 @@
 
 if [ $# -lt 1 ]
   then
-    echo "usage: $0 <enterprise slug> <hostname> <format: tsv|json> > output.tsv/json"
+    echo "usage: $0 <enterprise-slug> <hostname> <format: tsv|json> > output.tsv/json"
     exit 1
 fi
 
 enterpriseslug=$1
-hostname=$2
-format=$3
+hostname=${2:-"github.com"}
+format=${3:-"tsv"}
 export PAGER=""
 
-# set hostname to github.com by default
-if [ -z "$hostname" ]
-then
-  hostname="github.com"
-fi
-
-if [ -z "$format" ]
-then
-  format="tsv"
-fi
+# Define color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 organizations=$(gh api graphql --hostname $hostname --paginate -f enterpriseName="$enterpriseslug" -f query='
 query getEnterpriseOrganizations($enterpriseName: String! $endCursor: String) {
@@ -46,27 +39,46 @@ query getEnterpriseOrganizations($enterpriseName: String! $endCursor: String) {
 }' --jq '.data.enterprise.organizations.nodes[].login')
 
 # check to see if organizations is null - null error message is confusing otherwise
-if [ -z "$organizations" ] || [ $? -ne 0 ]
-then
-  # Define color codes
-  RED='\033[0;31m'
-  NC='\033[0m' # No Color
-
-  # Print colored messages
+if [ -z "$organizations" ] || [[ "$organizations" == *"INSUFFICIENT_SCOPES"* ]]; then
   echo -e "${RED}No organizations found for enterprise: $enterpriseslug${NC}"
-  echo -e "${RED}Check that you have the proper scopes for enterprise, e.g.: 'gh auth refresh -h github.com -s read:org -s read:enterprise'${NC}"
+  echo -e "${RED}  - Check that you have the proper scopes for enterprise with 'gh auth status' - you need at least 'read:enterprise'${NC}"
+  echo -e "${RED}  - You can run 'gh auth refresh -h github.com -s read:org -s read:enterprise' to add the scopes${NC}"
+  echo -e "${RED}  - Or you can run 'gh auth login -h github.com' and authenticate using a PAT with the proper scopes${NC}"
   exit 1
 fi
 
 if [ "$format" == "tsv" ]; then
-  echo -e "Org Login\tOrg Name\tOrg Desc\tDefault Repo Permission\tMembers Can Create Repos\t\tMembers Allowed Repos Creation Type\tMembers Can Create Public Repos\tMembers Can Create Private Repos\tMembers Can Create Internal Repos\tMembers Can Fork Private Repos"
+  echo -e "Organization Login\tDisplay Name\tDescription\tDefault Repo Permission\tMembers Can Create Repos\t\tMembers Allowed Repos Creation Type\tMembers Can Create Public Repos\tMembers Can Create Private Repos\tMembers Can Create Internal Repos\tMembers Can Fork Private Repos"
 fi
+
+errors=""
 
 for org in $organizations
 do
   if [ "$format" == "tsv" ]; then
-    gh api "orgs/$org" --hostname $hostname --jq ". | [\"$org\", .name, .description, .default_repository_permission, .members_can_create_repositories, .members_allowed_repository_creation_type, .members_can_create_public_repositories, .members_can_create_private_repositories, .members_can_create_internal_repositories, .members_can_fork_private_repositories] | @tsv"
+    output=$(gh api "orgs/$org" --hostname $hostname --jq ". | [\"$org\", .name, .description, .default_repository_permission, .members_can_create_repositories, .members_allowed_repository_creation_type, .members_can_create_public_repositories, .members_can_create_private_repositories, .members_can_create_internal_repositories, .members_can_fork_private_repositories] | @tsv" 2>&1)
   else
-    gh api "orgs/$org" --hostname $hostname
+    output=$(gh api "orgs/$org" --hostname $hostname 2>&1)
+  fi
+
+  if [ $? -ne 0 ] || [[ ! "$output" =~ "true" ]] && [[ ! "$output" =~ "false" ]]; then
+    if [ "$format" == "tsv" ]; then
+      output=$(echo -e "$output" | tr -d '\t')
+      output+=" - (you may not have admin access to the org)"
+    fi
+    errors="$errors\nError accessing organization: $org:\n$output"
+    if [ "$format" == "tsv" ]; then
+      echo -e "$org\tn/a\tn/a\tn/a\tn/a\tn/a\tn/a\tn/a\tn/a\tn/a"
+    else
+      echo -e "$org,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a"
+    fi
+  else
+    if [ -n "$output" ]; then
+      echo "$output"
+    fi
   fi
 done
+
+if [ -n "$errors" ]; then
+  echo -e "${RED}\nErrors encountered:\n$errors${NC}"
+fi
